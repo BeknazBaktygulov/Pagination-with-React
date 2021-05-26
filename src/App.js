28f3cdc793dbb86dbbd77184f75b890529ed5217
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-05-26 10:55:34 -0700

    Developed fetching data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import Posts from './components/Posts';
 
 function App() {
+  const [posts, setPosts ] = useState([]);
+  const [ loading, setLoading ] = useState(false);
+  const [currentPage, setCurrentPage ] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    async function fetchPosts () {
+      setLoading(true);
+      const result = await axios.get('https://jsonplaceholder.typicode.com/posts')
+      setPosts(result.data);
+      setLoading(false);
+    }
+    fetchPosts()
+  }, []);
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1 className="text-primary mb-3">My Blog</h1>
+          <Posts posts={posts} loading={loading} />
     </div>
   );
 }
